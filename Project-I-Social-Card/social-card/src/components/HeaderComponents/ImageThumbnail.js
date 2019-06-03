e59b51a54427d92e1b8e30cf64a9b787a9beca57
hcopasstarke <hcopasstarke@gmail.com>
2019-06-03 16:14:15 -0700

    branch + beginning

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../img/lambda.png';
+
+const ImageThumbnail = () => {
+    return (
+        <img id='profilePicture' src={logo} alt='profile image' />
+    )
+}
+
+export default ImageThumbnail;
+
