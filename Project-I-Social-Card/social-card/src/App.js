90e0bd66ae726dad6debc3516e9837e49ffc60d9
hcopasstarke <hcopasstarke@gmail.com>
2019-06-04 15:55:30 -0700

    styled

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import './App.css';
+import CardContainer from './components/CardComponents/CardContainer';
+
+
 
 const App = () => {
   return (
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+
+const NumberButton = props => {
+    return (
+    <button className={props.buttonStyle}>{props.text}</button>
+    );
+}
+
+NumberButton.defaultProps = {
+    buttonStyle: 'numberButton'
+}
+
+export default NumberButton; 
